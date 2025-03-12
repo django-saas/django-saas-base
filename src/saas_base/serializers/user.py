9430ceade1a59dbcf70e0c284d7e15a0b3603b14
Hsiaoming Yang <me@lepture.com>
2025-03-12 14:39:16 +0900

    feat: add translation locales

--- src/saas_base/serializers/user.py
+++ src/saas_base/serializers/user.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import password_validation
@@ -24,7 +25,7 @@ class UserPasswordSerializer(serializers.Serializer):
 
     def validate_password(self, raw_password: str):
         if self.initial_data["confirm_password"] != raw_password:
-            raise ValidationError("Password does not match.")
+            raise ValidationError(_("Password does not match."))
         password_validation.validate_password(raw_password)
         return raw_password
 
