3797f7e3f33ef9fdd8f9127d9f5d4a7454724176
Hsiaoming Yang <me@lepture.com>
2025-03-18 17:55:06 +0900

    fix: add old_password field in user password api

--- src/saas_base/serializers/user.py
+++ src/saas_base/serializers/user.py
@@ -1,6 +1,5 @@
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import password_validation
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -20,8 +19,15 @@ class UserEmailSerializer(serializers.ModelSerializer):
 
 
 class UserPasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
     confirm_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, value):
+        user = self.instance
+        if not user.check_password(value):
+            raise ValidationError(_("Password incorrect."))
+        return value
 
     def validate_password(self, raw_password: str):
         if self.initial_data['confirm_password'] != raw_password:
@@ -29,7 +35,7 @@ class UserPasswordSerializer(serializers.Serializer):
         password_validation.validate_password(raw_password)
         return raw_password
 
-    def update(self, user: AbstractUser, validated_data):
+    def update(self, user, validated_data):
         raw_password = validated_data['password']
         user.set_password(raw_password)
         user.save()
